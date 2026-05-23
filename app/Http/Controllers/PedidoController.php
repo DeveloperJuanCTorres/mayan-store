9fa3e07e5c369cdb79706c8007f6df29e3d84be8
Juan Carlos Torres del Castillo <jctorresdelcastillo@gmail.com>
2026-05-23 10:07:30 -0500

    listosss

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -108,7 +108,7 @@ class PedidoController extends Controller
                 Http::post($apis->ruta_whatsapp, [
                     "ruc_empresa" => $business->ruc,
                     "numero_celular" => $request->codigo . $request->telefono,
-                    "mensaje" => 'Gracias por su pedido, adjuntamos el detalle de compra.',
+                    "mensaje" => "Gracias por su pedido. Adjuntamos el detalle de su compra en PDF.\n\nSi desea, también podemos enviarle nuestras formas de pago y opciones disponibles para finalizar su pedido.",
                     "ruta_imagen" => config('app.url') . '/storage/pedidos/' . $pdfPath,
                     "apikey_bot" => $apis->apikey_bot_whatsapp,
                     "ruta_bot" => $apis->ruta_bot_whatsapp
